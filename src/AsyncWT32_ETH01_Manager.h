b938a7f7cfa4da1efbe53a8b173ddbe7b75738ce
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-12-08 22:57:01 -0500

    v1.0.0 for `ESP32 + LwIP LAN8720`
    #### Releases v1.0.0
    
    1. Initial coding to port [ESPAsync_WiFiManager](https://github.com/khoih-prog/ESPAsync_WiFiManager) to ESP32 boards using `LwIP LAN8720 Ethernet`.
    2. Use `allman astyle`

---
+++ src/AsyncWT32_ETH01_Manager.h
@@ -0,0 +1,33 @@
+/****************************************************************************************************************************
+  AsyncWT32_ETH01_Manager.h
+
+  For Ethernet shields using WT32_ETH01 (ESP32 + LAN8720)
+
+  AsyncWT32_ETH01_Manager is a library for the ESP32 with Ethernet LAN8720 to run Async Credential Manager
+
+  Modified from
+  1. Tzapu               (https://github.com/tzapu/WiFiManager)
+  2. Ken Taylor          (https://github.com/kentaylor)
+  3. Alan Steremberg     (https://github.com/alanswx/ESPAsyncWiFiManager)
+  4. Khoi Hoang          (https://github.com/khoih-prog/ESPAsync_WiFiManager)
+
+  Built by Khoi Hoang https://github.com/khoih-prog/AsyncWT32_ETH01_Manager
+  Licensed under MIT license
+
+  Version: 1.0.0
+
+  Version Modified By  Date      Comments
+  ------- -----------  ---------- -----------
+  1.0.0   K Hoang     09/12/2022 Initial coding for WT32_ETH01
+ *****************************************************************************************************************************/
+
+#pragma once
+
+#ifndef AsyncWT32_ETH01_Manager_h
+#define AsyncWT32_ETH01_Manager_h
+
+#include <AsyncWT32_ETH01_Manager.hpp>              //https://github.com/khoih-prog/AsyncWT32_ETH01_Manager
+#include <AsyncWT32_ETH01_Manager_Impl.h>           //https://github.com/khoih-prog/AsyncWT32_ETH01_Manager
+
+#endif    // AsyncWT32_ETH01_Manager_h
+
